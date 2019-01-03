093096889e4f26fa8db56eb0d73758167022b3f8
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-01-03 22:57:57 +0800

    Pass data from child to parent

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import genres from "./util/genres";
 
 new Vue({
   el: "#app",
+  methods: {
+      checkFilterRoot(filterCategory, filterTitle, filterCheckedStatus) {
+          this.$emit('check-filter', filterCategory, filterTitle, filterCheckedStatus);
+      }
+  },
   components: {
     "movie-list": {
       template: `<div id="movie-list">
@@ -25,10 +30,15 @@ new Vue({
           genres
         };
       },
+      methods: {
+        checkFilterFilter(filterCategory, filterTitle, filterCheckedStatus) {
+            this.$emit('check-filter', filterCategory, filterTitle, filterCheckedStatus);
+        }
+      },
       template: `<div id="movie-filter">
                         <h2>Filter Results</h2>
                         <div class="filter-group">
-                            <check-filter v-for="genre in genres" v-bind:title="genre"></check-filter>
+                            <check-filter v-for="genre in genres" v-bind:title="genre" @check-filter="checkFilterFilter"></check-filter>
                         </div>
                     </div>`,
       components: {
@@ -38,8 +48,15 @@ new Vue({
                 checked: false
               };
           },
+          methods: {
+            checkFilterChildren() {
+                this.checked = !this.checked;
+                // Param 1: custom event name, Param 2: the kind of filter which is genre, Param 3: props of interest, Param 4: data property 
+                this.$emit('check-filter', 'genre', this.title, this.checked);
+            }
+          },
           props: ["title"],
-          template: `<div v-bind:class="{ 'check-filter' : true, 'active' : checked }" @click="checked = !checked">
+          template: `<div v-bind:class="{ 'check-filter' : true, 'active' : checked }" @click="checkFilterChildren">
                         <span class="checkbox"></span>
                         <span class="check-filter-title">{{ title }}</span>
                     </div>`
