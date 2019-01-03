50d8495125d27b3c99fd4acf6477cc9afd082e86
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-01-03 23:20:00 +0800

    Add or remove checked genre from genre array

--- src/main.js
+++ src/main.js
@@ -4,9 +4,25 @@ import genres from "./util/genres";
 
 new Vue({
   el: "#app",
+  data: {
+    genre: [],
+    time: []
+  },
   methods: {
+    //   All of the data here obtained from onClick event from the child component and being passed to the parent
       checkFilterRoot(filterCategory, filterTitle, filterCheckedStatus) {
-          this.$emit('check-filter', filterCategory, filterTitle, filterCheckedStatus);
+        //   When filterCheckedStatus is true, add the corresponding filter title to genre array
+        // TODO: to further understand this part
+        if (filterCheckedStatus) {
+            this[filterCategory].push(filterTitle);
+        } else {
+            // if the filter category is being unchecked && the filter title is non-existent, then remove the filter title
+            let index = this[filterCategory].indexOf(filterTitle);
+            // if the title isnt present (hence, the value -1)
+            if (index > -1) {
+                this[filterCategory].splice(index, 1);
+            }
+        }
       }
   },
   components: {
