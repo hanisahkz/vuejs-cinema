2da8a762b6451eeb6371268162d20ad17a8d3950
Nurul Zaman <nurul.zaman@servicerocket.com>
2019-01-03 23:46:28 +0800

    Display movie based on the selected genre

--- src/main.js
+++ src/main.js
@@ -28,16 +28,39 @@ new Vue({
   components: {
     "movie-list": {
       template: `<div id="movie-list">
-                        <div v-for="movie in movies" class="movie">{{ movie.title }}</div>
+                        <div v-for="movie in filteredMovies" class="movie">{{ movie.title }}</div>
                     </div>`,
       data() {
         return {
           movies: [
-            { title: "Home Alone" },
-            { title: "Avengers" },
-            { title: "The Family" }
+            { title: "Home Alone", genre: genres.COMEDY },
+            { title: "Avengers", genre: genres.FANTASY },
+            { title: "The Family", genre: genres.DOCUMENTARY}
           ]
         };
+      },
+      props: ['genre', 'time'],
+      methods: {
+        // Callback function that accepts movie argument
+        moviePassesGenreFilter(movie) {
+            // If genre array is empty, return true which will display all movies
+            if (!this.genre.length) {
+                return true;
+            } else {
+                // If genre is being checked, compare the genre being checked against the genre of movies present and return appropriate boolean value
+                // this is using Array#find()
+                return this.genre.find(genre => movie.genre === genre);
+            }
+        }
+      },
+      // computed properties is used as the values involved need to be recalculated as DOM changes occur
+      computed: {
+          // Logic to only show the filtered movie list instead of all movies
+          filteredMovies() {
+              // This is using array filter method that accepts callback function
+              // The array contains list of movies
+              return this.movies.filter(this.moviePassesGenreFilter);
+          }
       }
     },
     "movie-filter": {
@@ -47,6 +70,7 @@ new Vue({
         };
       },
       methods: {
+        // TODO: consider to rename the variables
         checkFilterFilter(filterCategory, filterTitle, filterCheckedStatus) {
             this.$emit('check-filter', filterCategory, filterTitle, filterCheckedStatus);
         }
